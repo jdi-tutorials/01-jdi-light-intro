f71050c367b72dafc1442dd2c8b26d414e615c1a
RomanIovlev <Roman_Iovlev@epam.com>
2019-05-10 19:43:59 +0300

    small corrections

--- src/test/java/pageobject/TestsInit.java
+++ src/test/java/pageobject/TestsInit.java
@@ -10,6 +10,7 @@ import static com.epam.jdi.light.ui.html.PageFactory.initElements;
 public interface TestsInit {
     @BeforeSuite(alwaysRun = true)
     static void setUp() {
+        //logger.setLogLevel(STEP);
         initElements(SiteJdi.class);
     }
 }
