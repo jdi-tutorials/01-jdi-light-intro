b996c983a6d282284534f3746f9e51a7a45266f0
Roman_Iovlev <roman_iovlev@epam.com>
2019-10-12 11:14:39 +0300

    remove contacts

--- src/main/java/pageobject/uiobjects/example/jdisite/entities/ContactInfo.java
+++
@@ -1,8 +0,0 @@
-package pageobject.uiobjects.example.jdisite.entities;
-
-import com.epam.jdi.tools.DataClass;
-
-public class ContactInfo extends DataClass<ContactInfo> {
-    public String name, lastName, position, description;
-    public int passportNumber = -1, passportSeria = -1;
-}
